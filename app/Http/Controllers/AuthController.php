75653ca348342ec038f21e3fb5e9950ba560bfc2
19523197 <19523197@students.uii.ac.id>
2022-09-26 11:30:31 +0700

    jwt token fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -80,6 +80,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
+            'user' => auth()->user(),
             'expires_in' => Auth::factory()->getTTL() * 60
         ]);
     }
